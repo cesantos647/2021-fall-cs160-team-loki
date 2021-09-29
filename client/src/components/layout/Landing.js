7f2c1eed325d7990dc2f2c62130baeba629093d7
james <jnguy2018@gmail.com>
2021-09-29 16:36:26 -0700

    modified boilerplate code for our app

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,13 +8,12 @@ class Landing extends Component {
         <div className="row">
           <div className="col s12 center-align">
             <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
+              <b>Get started</b> with {" "}
+              <span style={{ fontFamily: "monospace" }}>Sanvac</span>
             </h4>
             <p className="flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
+               With our intuitive learning management system, you'll have all the tools you need to succeed - without the hassle.
+               You're just one click away.
             </p>
             <br />
             <div className="col s6">
