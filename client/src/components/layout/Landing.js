c690a7f2a0400a4befbfd1b1f2be498662ea700b
james <jnguy2018@gmail.com>
2021-10-11 21:54:17 -0700

    initial

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -6,18 +6,18 @@ class Landing extends Component {
     return (
       <div style={{ height: "75vh" }} className="container valign-wrapper">
         <div className="row">
-          <div className="col s12 center-align">
+          <div className="text-4xl col s12 center-align">
             <h4>
               <b>Build</b> a login/auth app with the{" "}
               <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
               scratch
             </h4>
-            <p className="flow-text grey-text text-darken-1">
+            <p className="text-5xl flow-text grey-text text-darken-1">
               Create a (minimal) full-stack app with user authentication via
               passport and JWTs
               </p>
             <br />
-            <div className="col s6">
+            <div className="rounded col s6">
               <Link
                 to="/register"
                 style={{
@@ -25,7 +25,7 @@ class Landing extends Component {
                   borderRadius: "3px",
                   letterSpacing: "1.5px"
                 }}
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+                className="rounded btn btn-large waves-effect waves-light hoverable blue accent-3"
               >
                 Register
               </Link>
