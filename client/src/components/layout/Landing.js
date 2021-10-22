3fdf590bde7954c93f8ffd74c7bf2fe519765b0e
james <jnguy2018@gmail.com>
2021-10-21 18:33:16 -0700

    Created landingPage template

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,51 +1,31 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import Background from '../../images/towerhall.jpg';
+
+const bgImage = {
+  backgroundImage: `url(${Background})`
+}
 
 class Landing extends Component {
   render() {
     return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="text-4xl col s12 center-align">
-            <h4>
-              <b>Build</b> a login/auth app with the{" "}
-              <span style={{ fontFamily: "monospace" }}>MERN</span> stack from
-              scratch
-            </h4>
-            <p className="text-5xl flow-text grey-text text-darken-1">
-              Create a (minimal) full-stack app with user authentication via
-              passport and JWTs
-              </p>
-            <br />
-            <div className="rounded col s6">
-              <Link
-                to="/register"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="rounded btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Register
-              </Link>
+        <div style={bgImage} className="bg-tower-hall bg-cover h-screen w-screen grid grid-cols-12">
+          <div className="flex col-start-1 col-end-5 items-center">
+            <div className="flex bg-blue-900 w-full h-1/5 justify-center items-center border border-opacity-80 border-white">
+              <div className="grid gap-y-4 p-4 text-3xl text-white truncate">
+                <h1><b className="text-green-300">Better</b> Community.</h1>
+                <h1><b className="text-green-300">Better</b> Education.</h1>
+                <p className="text-sm">Register and get virtually connected to your school's resources.</p>
+              </div>
             </div>
-            <div className="col s6">
-              <Link
-                to="/login"
-                style={{
-                  width: "140px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px"
-                }}
-                className="btn btn-large btn-flat waves-effect white black-text"
-              >
-                Log In
-              </Link>
+          </div>
+          <div className="flex justify-center col-start-10 col-end-13 bg-gray-50">
+            <div className="text-6xl">
+              <h1 className="inline text-green-300">SAV</h1>
+              <h1 className="inline text-blue-900">NAC</h1>
             </div>
           </div>
         </div>
-      </div>
     );
   }
 }
