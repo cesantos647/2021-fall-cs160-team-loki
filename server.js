c4cadfeed5d975732a4bd9df5a074bb919d5112c
Christian Santos <cesantos647@gmail.com>
2021-10-27 00:30:56 -0700

    CS160-35 - Create Chat REST Objects

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const passport = require("passport");
 const users = require("./routes/api/users");
 const courses = require("./routes/api/courses");
+const chats = require("./routes/api/chats");
+const chatrooms = require("./routes/api/chatrooms");
 
 // Bodyparser middleware
 app.use(
@@ -28,9 +30,12 @@ mongoose
 app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
+
 // Routes
 app.use("/api/users", users);
 app.use("/api/courses", courses);
+app.use("/api/chats", chats);
+app.use("/api/chatrooms", chatrooms);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
