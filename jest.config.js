357da026291238f2968a14f77bebea9d838971c3
ChrisLouie10 <christopherlouie16@gmail.com>
2021-11-07 10:45:53 -0800

    reverting merge

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-const jestConfig = {
-  verbose: true,
-  testURL: "http://localhost:5000/",
-  testMatch: ['**/tests/*.js?(x)'],
-  testEnvironment: "node"
-}
-
-module.exports = jestConfig
--- server.js
+++ server.js
@@ -41,5 +41,3 @@ app.use("/api/chatrooms", chatrooms);
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
-
-module.exports = app
