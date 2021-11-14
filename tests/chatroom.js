1c9a96b4b0923fefea27ffe5cd555dc9bef999f2
ChrisLouie10 <christopherlouie16@gmail.com>
2021-11-14 14:15:43 -0800

    fixed syntax error and added tests to chatroom

--- tests/chatroom.js
+++ tests/chatroom.js
@@ -5,7 +5,6 @@ const Chatroom = require("../models/Chatroom");
 const jwtDecode = require('jwt-decode');
 
 var userId;
-var chatRoomId;
 var testChatIds = ["chatId1", "chatId2"];
 var testUserIds = ["userId1", "userId2"];
 
@@ -34,6 +33,7 @@ beforeAll(async () => {
 });
 
 describe("POST/GET for chatroom", () => {
+  let chatRoomId;
   test("testing POST chatroom", () => {
     return request(app)
       .post("/api/chatrooms/chatroom")
@@ -58,12 +58,33 @@ describe("POST/GET for chatroom", () => {
         })
       });
   })
+  test("testing add chat to chatroom", () => {
+    let chatId = "testChatId3";
+    testChatIds.push("testChatId3");
+    return request(app)
+      .post("/api/chatrooms/addChat/" + chatRoomId + "/" + chatId)
+      .then(response => {
+        console.log(response.body)
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body.data.chatRoom.users)).toBe(true);
+        expect(Array.isArray(response.body.data.chatRoom.chats)).toBe(true);
+        response.body.data.chatRoom.users.map((userId, index) => {
+          expect(userId).toBe(testUserIds[index]);
+        })
+        response.body.data.chatRoom.chats.map((message, index) => {
+          expect(message).toBe(testChatIds[index]);
+        })
+        testChatIds.pop()
+      });
+  })
   afterAll(async () => {
     await Chatroom.deleteOne({ _id: chatRoomId });
   })
 });
 
 describe("Testing incorrect datatypes", () => {
+  let chatRoomId;
+  
   afterEach(async () => {
     await Chatroom.deleteOne({ _id: chatRoomId });
   });
@@ -73,6 +94,7 @@ describe("Testing incorrect datatypes", () => {
       .post("/api/chatrooms/chatroom")
       .send({ users: [1, 2], chats: testChatIds })
       .then(response => {
+        if(response.statusCode == 200) chatRoomId = response.body.data.chatRoomId;
         expect(response.statusCode).toBe(400);
       });
   });
@@ -81,6 +103,7 @@ describe("Testing incorrect datatypes", () => {
       .post("/api/chatrooms/chatroom")
       .send({ users: testUserIds, chats: [1, 2] })
       .then(response => {
+        if(response.statusCode == 200) chatRoomId = response.body.data.chatRoomId;
         expect(response.statusCode).toBe(400);
       });
   });
@@ -89,6 +112,7 @@ describe("Testing incorrect datatypes", () => {
       .post("/api/chatrooms/chatroom")
       .send({ users: "testUserId", chats: testChatIds })
       .then(response => {
+        if(response.statusCode == 200) chatRoomId = response.body.data.chatRoomId;
         expect(response.statusCode).toBe(400);
       });
   });
@@ -97,6 +121,7 @@ describe("Testing incorrect datatypes", () => {
       .post("/api/chatrooms/chatroom")
       .send({ users: testUserIds, chats: "testChatId" })
       .then(response => {
+        if(response.statusCode == 200) chatRoomId = response.body.data.chatRoomId;
         expect(response.statusCode).toBe(400);
       });
   });
