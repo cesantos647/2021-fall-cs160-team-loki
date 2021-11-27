74d65b22d67129edb1a170bc38371a2c653e79c5
Hoai-Nam Phung <hoai-nam.phung@sjsu.edu>
2021-11-26 21:36:10 -0800

    Nested components within routes in App.js, CourseRouter.js. Changed CourseSidebar to be a function with hooks, rather than a class. Implemented route based courseId retrieval and placement to and from URL in CourseRouter.js, CourseSidebar.js.

--- client/src/actions/courseActions.js
+++ client/src/actions/courseActions.js
@@ -1,13 +1,30 @@
 import axios from "axios";
+import {GET_ERRORS} from "./types"
 
 export const getCourseDetails = (courseId, history) => {
-    return axios
-      .get(`/api/courses/${courseId}`)
-      .then(res => {
-        return res.data.data
+  return axios
+    .get(`/api/courses/${courseId}`)
+    .then(res => {
+      return res.data.data
+    })
+    .catch(err =>
+      // Do something here
+      console.log(err)
+    );
+};
+
+export const createCourse = (courseData, history) => dispatch => {
+  axios
+    .post("/api/courses", courseData)
+    .then(res => {
+      let courseId = res.data.data.courseId
+      console.log(courseId);
+      history.push(`/courses/${courseId}/chat`)
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
       })
-      .catch(err =>
-        // Do something here
-        console.log(err)
-      );
+    );
 };
--- client/src/components/course/CourseCreation.js
+++ client/src/components/course/CourseCreation.js
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import FormField from "../form/FormField";
+import classnames from "classnames";
 import FormButton from "../form/FormButton";
+import { createCourse } from "../../actions/courseActions"
 
 class CourseCreation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      courseName: "",
+      courseSection: "",
+      errors: {}
+    };
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newCourse = {
+      courseName: this.state.courseName,
+      courseSection: this.state.courseSection,
+    };
+
+    this.props.createCourse(newCourse, this.props.history);
+  };
+
   render() {
+    const { errors } = this.state;
+
     return (
       <div className="h-screen px-12 pt-4 ml-16 bg-gray-800">
         <form noValidate onSubmit={this.onSubmit}>
@@ -15,10 +50,43 @@ class CourseCreation extends Component {
               <span className="text-gray-200"> Creation</span>
             </label>
           </div>
-          <FormField label="Course Name"/>
-          <FormField label="Course Section"/>
+
+          <div className="my-4">
+            <label class="block text-gray-300 text-sm font-bold mb-2">
+              Course Name
+            </label>
+            <input
+              onChange={this.onChange}
+              value={this.state.courseName}
+              error={errors.error}
+              id="courseName"
+              type="text"
+              className={classnames("shadow appearance-none border bg-gray-200 focus:bg-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline", {
+                invalid: errors.error
+              })} />
+          </div>
+
+          <div className="my-4">
+            <label class="block text-gray-300 text-sm font-bold mb-2">
+              Course Section
+            </label>
+            <input
+              onChange={this.onChange}
+              value={this.state.section}
+              error={errors.section}
+              id="courseSection"
+              type="text"
+              className={classnames("shadow appearance-none border bg-gray-200 focus:bg-white rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline", {
+                invalid: errors.error
+              })} />
+          </div>
+
+          <span className="text-xs italic text-red-500">
+            {errors.error}
+          </span>
+
           <div className="mt-8">
-            <FormButton label="Create" color="blue"/>
+            <FormButton label="Create" color="blue" />
           </div>
         </form>
       </div>
@@ -26,5 +94,16 @@ class CourseCreation extends Component {
   }
 }
 
+CourseCreation.propTypes = {
+  createCourse: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
 export default connect(
+  mapStateToProps,
+  { createCourse }
 )(CourseCreation);
